cd9b4a2bdcdba4299afba701eaf12152328a344a
Matheus G. Bento <matgoben@gmail.com>
2026-07-07 12:12:48 -0300

    ajuste conecções

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
   out: './src/database/migrations',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL || '',
+    url: process.env.AMPAG_DATABASE_URL || '',
   },
 });
--- src/database/connection.ts
+++ src/database/connection.ts
@@ -3,9 +3,9 @@ import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import * as schema from "./schema";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is not defined. Please set it in your .env file.");
+if (!process.env.AMPAG_DATABASE_URL) {
+  throw new Error("AMPAG_DATABASE_URL is not defined. Please set it in your .env file.");
 }
 
-const sql = neon(process.env.DATABASE_URL);
+const sql = neon(process.env.AMPAG_DATABASE_URL);
 export const db = drizzle(sql, { schema });
